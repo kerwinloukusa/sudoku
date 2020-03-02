dad2b71a88e9b496b4f3040bb10c1fa802aa83ed
kerwin loukusa <kerwinloukusa@gmail.com>
2020-03-02 14:24:49 -0800

    updates to helper functions
    updated helper functions to fix problems with backtracking/recurision code that was not working properly. I believe the current version works well, but may not be the most efficent.

--- sudoku/core.py
+++ sudoku/core.py
@@ -1,9 +1,9 @@
 import numpy as np
-import sudoku_functions as su
+import helpers as su
 import pandas as pd
-df=pd.read_csv('sudoko.csv', sep=',',header=None)
+df=pd.read_csv('puzzles/1.csv', sep=',',header=None)
 df.style.hide_index()
 arr = df.to_numpy()
 
 #print(arr)
-su.solvePuzzle(arr,0)
+su.solvePuzzle(arr)
--- sudoku/helpers.py
+++ sudoku/helpers.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-def checkRows (arr):
+def checkRows (array):
     for x in range(9):
         seen = set()
-        row = arr[x,:]
+        row = array[x,:]
         for n in row:
             if n != 0:
                 if n in seen:
@@ -12,10 +12,10 @@ def checkRows (arr):
                     seen.add(n) 
     return True
 
-def checkColumns (arr):
+def checkColumns (array):
     for x in range(9):
         seen = set()
-        column = arr[:,x]
+        column = array[:,x]
         for n in column:
             if n != 0:
                 if n in seen:
@@ -25,11 +25,11 @@ def checkColumns (arr):
                     seen.add(n)    
     return True
 
-def checkSquares(arr):
+def checkSquares(array):
     for row in range(3):
         for column in range(3):
             seen = set()
-            square = arr[(row*3):(row*3+3), (column*3):(column*3+3)].flatten()
+            square = array[(row*3):(row*3+3), (column*3):(column*3+3)].flatten()
             for n in square:
                 if n != 0:
                     if n in seen:
@@ -38,19 +38,38 @@ def checkSquares(arr):
                         seen.add(n)  
     return True
 
-def checkPuzzle (arr):
-    return (checkRows(arr) & checkColumns(arr) & checkSquares(arr))
 
-def solvePuzzle (array, count):
+def checkPuzzle (array):
+    return (checkRows(array) & checkColumns(array) & checkSquares(array))
+
+def findNextZero (array):
+    row = 0;
+    column = 0;
+    solved = False
     if len(np.where(array == 0)[0]) != 0:
-            for x in range(9):
-                row = array[x,:].flatten()
-                for n in range(9):
-                    #print (array)
-                    if row[n] == 0:
-                        for y in range(1,10):
-                            array[x,n] = y
-                            if checkPuzzle(array):
-                                solvePuzzle (array,(count+1))
+        for x in range(9):
+            for y in range(9):
+                if array[x,y] == 0:
+                    row = x
+                    column = y 
+                    solved = True
+                    return [row, column, solved]
+                
     else:
+        return [row, column, solved]
+
+# each new cell needs to be a new function call?!..
+def solvePuzzle (array):
+    lis = findNextZero(array)
+    if lis[2] == False:
         print(array)
+        return True
+    row = lis[0]
+    column = lis[1]
+    for n in range(1,10):
+        array[row,column] = n
+        if checkPuzzle(array):
+            if solvePuzzle(array):
+                return True
+    array[row,column] = 0
+    return False            
