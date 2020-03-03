2293591d7dbd1716e36c9881839893c811a41fa4
kerwin loukusa <kerwinloukusa@gmail.com>
2020-03-02 19:28:09 -0800

    updates

--- sudoku/core.py
+++ sudoku/core.py
@@ -1,9 +1,8 @@
 import numpy as np
 import helpers as su
 import pandas as pd
-df=pd.read_csv('puzzles/1.csv', sep=',',header=None)
+df=pd.read_csv('puzzles/2.csv', sep=',',header=None)
 df.style.hide_index()
-arr = df.to_numpy()
 
-#print(arr)
-su.solvePuzzle(arr)
+array_puzzle = df.to_numpy()
+su.solvePuzzle(array_puzzle)
--- sudoku/helpers.py
+++ sudoku/helpers.py
@@ -1,5 +1,47 @@
 import numpy as np
 
+# main function to solve the puzzle
+def solvePuzzle (array):
+    
+    # find next zero in puzzle, if there is no zero that indicates the puzzle has been solved
+    lis = findNextZero(array)
+    if lis[2] == False:
+        print(array)
+        return True
+    row = lis[0]
+    column = lis[1]
+    
+    # backtracking algorithim, attempt a candidate number if the solution is feasible proceed
+    # by calling the function call again and continue until a solution is found
+    for n in range(1,10):
+        array[row,column] = n
+        if checkPuzzle(array):
+            if solvePuzzle(array):
+                return True
+    array[row,column] = 0
+    return False 
+
+# finds the next zero in the puzzle grid and returns it to the puzzle solver
+def findNextZero (array):
+    row = 0;
+    column = 0;
+    solved = False
+    if len(np.where(array == 0)[0]) != 0:
+        for x in range(9):
+            for y in range(9):
+                if array[x,y] == 0:
+                    row = x
+                    column = y 
+                    solved = True
+                    return [row, column, solved]
+                
+    else:
+        return [row, column, solved]
+
+# make sure that the puzzle rows/columns/squares are okay to proceed with solving
+def checkPuzzle (array):
+    return (checkRows(array) & checkColumns(array) & checkSquares(array))
+
 def checkRows (array):
     for x in range(9):
         seen = set()
@@ -39,37 +81,7 @@ def checkSquares(array):
     return True
 
 
-def checkPuzzle (array):
-    return (checkRows(array) & checkColumns(array) & checkSquares(array))
 
-def findNextZero (array):
-    row = 0;
-    column = 0;
-    solved = False
-    if len(np.where(array == 0)[0]) != 0:
-        for x in range(9):
-            for y in range(9):
-                if array[x,y] == 0:
-                    row = x
-                    column = y 
-                    solved = True
-                    return [row, column, solved]
-                
-    else:
-        return [row, column, solved]
 
-# each new cell needs to be a new function call?!..
-def solvePuzzle (array):
-    lis = findNextZero(array)
-    if lis[2] == False:
-        print(array)
-        return True
-    row = lis[0]
-    column = lis[1]
-    for n in range(1,10):
-        array[row,column] = n
-        if checkPuzzle(array):
-            if solvePuzzle(array):
-                return True
-    array[row,column] = 0
-    return False            
+
+           
