b40430fd2f4effc56c3aa750ae66b3211c759001
kerwinloukusa <kerwinloukusa@gmail.com>
2022-02-28 16:51:54 -0800

    updates to solver to use better method to check puzzle

--- sudoku/core.py
+++ sudoku/core.py
@@ -1,8 +1,21 @@
 import numpy as np
 import helpers as su
 import pandas as pd
-df=pd.read_csv('puzzles/2.csv', sep=',',header=None)
+import time
+
+
+start = time.time()
+
+#read puzzle
+df=pd.read_csv('puzzles/1.csv', sep=',',header=None)
 df.style.hide_index()
 
+
+#convert to numpy array and solve
 array_puzzle = df.to_numpy()
 su.solvePuzzle(array_puzzle)
+
+
+end = time.time()
+print(end - start)
+
--- sudoku/helpers.py
+++ sudoku/helpers.py
@@ -25,59 +25,34 @@ def solvePuzzle (array):
 def findNextZero (array):
     row = 0;
     column = 0;
-    solved = False
     if len(np.where(array == 0)[0]) != 0:
         for x in range(9):
             for y in range(9):
                 if array[x,y] == 0:
                     row = x
                     column = y 
-                    solved = True
-                    return [row, column, solved]
+                    return [row, column, True]
                 
     else:
-        return [row, column, solved]
+        return [row, column, False]
 
-# make sure that the puzzle rows/columns/squares are okay to proceed with solving
-def checkPuzzle (array):
-    return (checkRows(array) & checkColumns(array) & checkSquares(array))
+# iterate through puzzle and make sure each row, column, and 3x3 box has no duplicates
+def checkPuzzle(array):
+    s = set()
+    for x in range(0,len(array)):
+        for y in range(0,len(array)):
+            if array[x,y] == 0:
+                continue
+            row_str = "row_" + str(x) + "_val_" + str(array[x][y])
+            col_str = "col_" + str(y) + "_val_" + str(array[x][y])
+            mat_str = "mat_" + str(x//3)+ "_" + str(y//3) + "_val_" + str(array[x][y])
+            if row_str not in s and col_str not in s and mat_str not in s:
+                s.add(row_str)
+                s.add(col_str)
+                s.add(mat_str)
+            else:
+                return False
 
-def checkRows (array):
-    for x in range(9):
-        seen = set()
-        row = array[x,:]
-        for n in row:
-            if n != 0:
-                if n in seen:
-                    return False
-                else:
-                    seen.add(n) 
-    return True
-
-def checkColumns (array):
-    for x in range(9):
-        seen = set()
-        column = array[:,x]
-        for n in column:
-            if n != 0:
-                if n in seen:
-                    
-                    return False
-                else:
-                    seen.add(n)    
-    return True
-
-def checkSquares(array):
-    for row in range(3):
-        for column in range(3):
-            seen = set()
-            square = array[(row*3):(row*3+3), (column*3):(column*3+3)].flatten()
-            for n in square:
-                if n != 0:
-                    if n in seen:
-                        return False
-                    else:
-                        seen.add(n)  
     return True
 
 
